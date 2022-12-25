f495fe2e9ed8da4a83f26acc333384fea0d17c5f
muhammad Nurul Musthofa <99570369+muhammadnurulmusthofa@users.noreply.github.com>
2022-12-26 06:16:42 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,14 @@
-
+<?php 
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan'] == "gagal"){
+			echo "Login gagal.. email dan password salah!";
+		}else if($_GET['pesan'] == "logout"){
+			echo "Anda telah berhasil logout";
+		}else if($_GET['pesan'] == "belum_login"){
+			echo "Anda harus masuk login untuk mengakses halaman admin";
+		}
+	}
+	?>
 
 
 <!DOCTYPE html>
