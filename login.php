20d3d5fec09e4bba72007997b8f8f3a6638e7b1a
muhammad Nurul Musthofa <99570369+muhammadnurulmusthofa@users.noreply.github.com>
2022-12-26 06:14:31 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,33 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Form Login</title>
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css'><link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="container">
+	<div id="login-box">
+		<div class="logo">
+			<img src="" class="img img-responsive img-circle center-block"/>
+			<h1 class="logo-caption"><span class="tweak">L</span>ogin</h1>
+		</div><!-- /.logo -->
+		<div class="controls">
+			<input type="text" name="username" placeholder="Username" class="form-control" />
+			<input type="text" name="username" placeholder="Password" class="form-control" />
+            <a href="index.html"><button type="button" class="btn btn-default btn-block btn-custom">Login</button></a>
+			
+	</div>
+</div>
+<div id="particles-js"></div>
+<!-- partial -->
+  <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css'></script>
+  <script src='https://code.jquery.com/jquery-1.11.1.min.js'></script><script  src="script.js"></script>
+
+</body>
+</html>
